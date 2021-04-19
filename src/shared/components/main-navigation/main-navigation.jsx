466911dc6fc983e6feab9ba9b341847e75ce7f92
thomaslebn <lebn.thomas@pm.me>
2021-04-19 16:19:10 +0200

    Adding some adjustments

--- src/shared/components/main-navigation/main-navigation.jsx
+++ src/shared/components/main-navigation/main-navigation.jsx
@@ -31,7 +31,7 @@ const MainNavigation = () => {
           <span />
         </button>
         <h1 className="main-navigation__title">
-          <Link to="/"> Your Places </Link>
+          <Link to="/"> Share'Out </Link>
         </h1>
         <nav className="main-navigation__header-nav">
           <MainLinks />
